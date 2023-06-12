8e824f5139b752506065133fff6531f706be7529
oleg.ivakin <oleg_ivakin@mail.ru>
2023-06-12 22:59:10 +0600

    Example002_HelloUser

---
+++ Examples/Example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Сonsole.WriteLine("Введите Ваше имя");
+//string username = Console.ReadLine();
+//Сonsole.WriteLine("Привет, ");
+//Сonsole.WriteLine(username);
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
