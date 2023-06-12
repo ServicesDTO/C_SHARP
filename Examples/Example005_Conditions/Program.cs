b996febf1f8f2b43510501739a4e154e68f5d712
oleg.ivakin <oleg_ivakin@mail.ru>
2023-06-12 23:22:29 +0600

    Example005_Conditions

---
+++ Examples/Example005_Conditions/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша"){
+
+Console.WriteLine("Урра это Маша!");
+}
+else 
+{
+    Console.WriteLine("Привет, " + username);
+}
+
+
